43c6eb35b4c39953386733a4cbde81fc1aa38513
Gaurav Jha <154802200+GauravJha319@users.noreply.github.com>
2025-04-11 23:54:17 +0530

    Time: 79 ms (29.91%) | Memory: 10.4 MB (22.77%) - LeetSync

---
+++ 2998-count-symmetric-integers/count-symmetric-integers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isSymmetric(int num)
+    {
+        string s = to_string(num);
+        int n = s.length();
+
+        if (n % 2 != 0) return false;
+
+        int half = n / 2;
+        int sum1 = 0, sum2 = 0;
+
+        for (int i = 0; i < half; ++i)
+        {
+            sum1 += s[i] - '0';
+            sum2 += s[i + half] - '0';
+        }
+        return sum1 == sum2;
+    }
+    int countSymmetricIntegers(int low, int high)
+    {
+        int count = 0;
+        for (int i = low; i <= high; ++i)
+        {
+            if (isSymmetric(i))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
