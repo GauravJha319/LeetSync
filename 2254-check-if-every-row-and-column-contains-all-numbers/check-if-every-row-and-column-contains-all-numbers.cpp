41262007e9869b6d66653c1d15127c1c841ee649
Gaurav Jha <154802200+GauravJha319@users.noreply.github.com>
2025-07-22 19:47:00 +0530

    Time: 173 ms (10.14%) | Memory: 95.6 MB (25.39%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -8,9 +8,11 @@ public:
             unordered_map<int,int> freq1;
             for (int j=0 ; j<n ; j++)
             {
-                int temp=matrix[i][j];
                 freq1[matrix[i][j]]++;
-                if (freq1[temp]>1) return false;
+            }
+            for (int k=1 ; k<n ; k++)
+            {
+                if (freq1[k]!=1) return false;
             }
         }
         for (int i=0 ; i<n ; i++)
@@ -18,9 +20,11 @@ public:
             unordered_map<int,int> freq2;
             for (int j=0 ; j<n ; j++)
             {
-                int temp=matrix[j][i];
                 freq2[matrix[j][i]]++;
-                if (freq2[temp]>1) return false;
+            }
+            for (int k=1 ; k<n ; k++)
+            {
+                if (freq2[k]!=1) return false;
             }
         }
         return true;
